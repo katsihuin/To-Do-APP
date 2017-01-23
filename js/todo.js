c03752239b17391a107bc95d808c0dd5756d02f1
Katherin Sihuin <katsihuin@gmail.com>
2017-01-23 15:53:10 -0500

    first version of exercise

--- js/todo.js
+++ js/todo.js
@@ -1,12 +1,11 @@
-// Exercise: Create a to do list application
+//variable global
+var inItemTask = document.getElementById("todoInput");
 
-// todoList function used on button click to get user input and 
-
+//funcion para actualizar el estado del checkbox
 function updateTaskStatus()
 {
 	var checkId = this.id.replace("cb_", "");
 	var itemTask = document.getElementById("item_" + checkId);
-
 
 	if (this.checked) 
 	{
@@ -18,45 +17,11 @@ function updateTaskStatus()
 	}
 }
 
-function addNewTask(list, itemTask)
-{	
-	
-	totalItems++;
-	var totalItems = 0;
-
-	// Para conseguir id unicos
-	//var date = new Date ();
-	//var id = "" + date.getHours() + date.getMinutes() + date.getSecond(); + date.getMilliseconds();
-
-	//Crea una nueva tarea con un checkbox al inicio
-	var listTask = document.createElement("li");
-	//listTask.id = "li_" + id;
-	var checkbox = document.createElement("input");
-	checkbox.type = "checkbox";
-	checkbox.id = "cb_" + totalItems;
-	checkbox.onclick = updateTaskStatus;
-
-	var span = document.createElement("span");
-	span.id = "item_" + totalItems;
-	span.innerHTML = itemTask;
-	//span.ondblclick = renameItem;
-
-	var icon = document.createElement("i");
-	icon.className= "fa fa-trash-o pull-right";
-
-	//icon.onclick = removeTask;
-	  
-	//listTask.innerHTML = itemTask;
-	listTask.appendChild(checkbox);
-	listTask.appendChild(span);
-	listTask.appendChild(icon);
-	list.appendChild(listTask);
-
-}
-/*function renameItem 
+//funcion para editar las tareas
+function renameItem() 
 {
 	// Cuando se da double click a la tarea ubicada en span
-	var newText = prompt(¿Cúal deberia ser el nombre de esta tarea);
+	var newText = prompt("¿Cúal deberia ser el nombre de esta tarea?");
 
 	if (!newText || newText == "" || newText == " ")
 	{
@@ -64,20 +29,56 @@ function addNewTask(list, itemTask)
 	}
 	this.innerHTML = newText;
 }
-function removeTask 
+
+//funcion para editar las tareas
+function removeTask(evt) 
 {
 	// Cuando se da click al icono tacho ubicada en icon
-	var iconId = this.id.replace("item_", "");
-	document.getElementById("li_" + iconId).style.display = "none";
+	var hijo = evt.target.parentNode;
+	hijo.parentNode.removeChild(hijo);
+}
 
-	var item =this.parentNode.parentNode;
-	var parent = item.parentNode;
+//funcion para agrega tareas
+function addNewTask(list, itemTask)
+{	
+	// Para conseguir id unicos
+	var date = new Date ();
+	var id = "" + date.getHours() + date.getMinutes() + date.getSeconds(); + date.getMilliseconds();
 
-	parent.removeChild(item);
-}*/
+	//Crea una nueva tarea 
+	var listTask = document.createElement("li");
+	listTask.id = "li_" + id;
 
-var inItemTask = document.getElementById("todoInput");
-inItemTask.focus();
+	//Adiciona un checkbox a la tarea
+	var checkbox = document.createElement("input");
+	checkbox.type = "checkbox";
+	checkbox.id = "cb_" + id;
+	checkbox.onclick = updateTaskStatus;
+
+	//Adiciona un span la tarea para el texto
+	var span = document.createElement("span");
+	span.id = "item_" + id;
+	span.innerHTML = itemTask;
+	span.ondblclick = renameItem;
+
+	//Adiciona un icono tacho la tarea para eliminar la tarea
+	var icon = document.createElement("i");
+	icon.id = "icon_"
+	icon.className = "fa fa-trash-o pull-right";
+	icon.onclick = removeTask;
+	
+	listTask.appendChild(checkbox);
+	listTask.appendChild(span);
+	listTask.appendChild(icon);
+	list.appendChild(listTask);
+
+	//enfoca el cursor en el text area
+	inItemTask.focus();
+
+	//Borra el texto del text area
+	inItemTask.value="";
+}
+
 
 // Agregar tareas con click al boton agregar
 var btnNew = document.getElementById("btnAdd");
@@ -88,7 +89,9 @@ btnNew.onclick = function()
 	if(itemTask == "" || itemTask == " ")
 	{
 		return false;
+		alert("Ingresa una tarea a completar y luego click en el botón Agregar o presiona ENTER");
 	}
+
 
 addNewTask(document.getElementById("todoList"), itemTask);
 }
